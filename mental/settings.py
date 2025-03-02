e2926b85f6b501c2185b5e10b594366e1f5029a5
Ruddy <rudraparbat714@gmail.com>
2025-03-03 00:47:59 +0530

    added origin

--- mental/settings.py
+++ mental/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 from datetime import timedelta
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -33,10 +34,12 @@ ALLOWED_HOSTS = ["*"]
 CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",  # 
+    "http://localhost:5173", 
+    "http://localhost:5174", # 
 ]
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:5173",  
+    "http://localhost:5174",
 ]
 # Application definition
 
