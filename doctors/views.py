ac5c9ae697c93463a32e984e9e76a1d241bcd6fc
Ruddy <rudraparbat714@gmail.com>
2025-04-06 14:41:04 +0530

    add 7 days max age tokens

--- doctors/views.py
+++ doctors/views.py
@@ -27,8 +27,8 @@ class CreateUserProfile(APIView) :
                     "user_id" : serializer.data['id'],
                 }
             )
-            response.set_cookie("access_token", value=access_token, httponly=True, secure=True, samesite='None')
-            response.set_cookie("refresh_token", value=refresh_token, httponly=True, secure=True, samesite='None')
+            response.set_cookie("access_token", value=access_token, httponly=True, secure=True,max_age=60*60*24*7 ,  samesite='None')
+            response.set_cookie("refresh_token", value=refresh_token, httponly=True, secure=True, max_age=60*60*24*7,  samesite='None')
             print("Cookies set:", response.cookies)
             return response
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
